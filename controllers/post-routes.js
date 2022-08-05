f2f824bac14d85abd301e7a136a3caf51de7f329
ericrothmuller <eric@skymarketingco.com>
2022-08-04 22:13:49 -0700

    Added post-routs.js

---
+++ controllers/post-routes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const {Post,} = require("../models");
+
+router.post("/", async (req, res) => {
+  try {
+    if (!req.session.loggedIn) {
+      res
+        .status(400)
+        .json({message: "You must be logged in." });
+      return;
+    }
+
+    Post.create({
+      user_id: req.session.userId,
+      title: req.body.title,
+      body: req.body.body,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
